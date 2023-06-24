6e4afceacd05f40baee6db37801068f83b274dd3
Big Smoke <137207383+thesundowner1@users.noreply.github.com>
2023-06-24 22:01:33 +0300

    Update shogicode.js
    Added the alphabet with capitalization support

--- shogicode.js
+++ shogicode.js
@@ -1,39 +1,107 @@
-function shogi(str) 
-{
-  let encStr;
-  let strArr = [];
-  let encStrArr = [];
-	let char;
+function shogi(str) {
+            let strArr = [];
+            let encStrArr = [];
+            let encStr, char;
 
-  strArr = str.split("");
-  for (i = 0 ; i < strArr.length; i++)
-    { 
-       switch(strArr[i])
-				 {
-					 case "a":
-						 char = "0000";
-					 break;
-					 case "b":
-						 char = "0001";
-					 break;
-					 case "c":
-						 char = "0002";
-					 break;
+            str = str.toLocaleLowerCase();
+            strArr = str.split("");
 
+            for (i = 0; i < strArr.length; i++) {
+                switch (strArr[i]) {
 
-					 default:
-						 char = "9999";
-						 break;
-						
-						 
-				 }
-		 encStrArr.push(char);
-			console.log(encStrArr);
-			
-    }
-  
+                    case "a":
+                        char = "0000";
+                        break;
+                    case "b":
+                        char = "0001";
+                        break;
+                    case "c":
+                        char = "0002";
+                        break;
+                    case "d":
+                        char = "0003";
+                        break;
+                    case "e":
+                        char = "0010";
+                        break;
+                    case "f":
+                        char = "0011";
+                        break;
+                    case "g":
+                        char = "0012";
+                        break;
+                    case "h":
+                        char = "0013";
+                        break;
+                    case "i":
+                        char = "0020";
+                        break;
+                    case "j":
+                        char = "0021";
+                        break;
+                    case "k":
+                        char = "0022";
+                        break;
+                    case "l":
+                        char = "0023";
+                        break;
+                    case "m":
+                        char = "0030";
+                        break;
+                    case "n":
+                        char = "0031";
+                        break;
+                    case "o":
+                        char = "0032";
+                        break;
+                    case "i":
+                        char = "0033";
+                        break;
+                    case "p":
+                        char = "0040";
+                        break;
+                    case "q":
+                        char = "0041";
+                        break;
+                    case "r":
+                        char = "0042";
+                        break;
+                    case "s":
+                        char = "0043";
+                        break;
+                    case "t":
+                        char = "0050";
+                        break;
+                    case "u":
+                        char = "0051";
+                        break;
+                    case "v":
+                        char = "0052";
+                        break;
+                    case "w":
+                        char = "0053";
+                        break;
+                    case "x":
+                        char = "0060";
+                        break;
+                    case "y":
+                        char = "0061"
+                        break;
+                    case "z":
+                        char = "0062";
+                        break;
 
+                    default:
+                        char = "9999";
+                        break;
+                }
+                encStrArr.push(char);
 
-  
+            }
+            encStr = "";
+            for(i = 0; i <encStrArr.length ; i ++) {
+                encStr += String(encStrArr[i]);
 
-}
+            }
+            return encStr;
+        }
