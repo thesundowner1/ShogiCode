2d22737398002d4a4fdb148dbfd4592b98eb39c4
Big Smoke <137207383+thesundowner1@users.noreply.github.com>
2023-06-24 17:24:39 +0300

    phone commit 1
    Not finished

--- shogicode.js
+++ shogicode.js
@@ -1,1 +1,39 @@
+function shogi(str) 
+{
+  let encStr;
+  let strArr = [];
+  let encStrArr = [];
+	let char;
 
+  strArr = str.split("");
+  for (i = 0 ; i < strArr.length; i++)
+    { 
+       switch(strArr[i])
+				 {
+					 case "a":
+						 char = "0000";
+					 break;
+					 case "b":
+						 char = "0001";
+					 break;
+					 case "c":
+						 char = "0002";
+					 break;
+
+
+					 default:
+						 char = "9999";
+						 break;
+						
+						 
+				 }
+		 encStrArr.push(char);
+			console.log(encStrArr);
+			
+    }
+  
+
+
+  
+
+}
